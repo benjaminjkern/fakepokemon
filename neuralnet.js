ab13be08ef3df55dc5a3f9512a66bc0fb2aa00df
Ben Kern <bkern@arete.com>
2021-11-12 15:44:05 -0800

    tensors and quadratic network

--- neuralnet.js
+++ neuralnet.js
@@ -10,7 +10,7 @@ const mutateNeuralNet = (oldNeuralNet, mutation = 1) => {
 const newNeuralNet = (layerCounts, randomRange = 1) => ({
     name: Math.random().toString(36).substring(2),
     layers: layerCounts.slice(1).map((layerCount, i) => newLinearLayer(layerCounts[i], layerCount, randomRange, ...(i === layerCounts.length - 2 ? [] : [ReLU, dReLU]))),
-    layerCounts,
+    // layerCounts,
     pass(input) {
         return this.layers.reduce((p, layer) => layer.pass(p), input.map(i => [i])).flat();
     },
@@ -44,16 +44,20 @@ const newNeuralNet = (layerCounts, randomRange = 1) => ({
     }
 });
 
+const newQuadraticNeuralNet = (layerCounts, randomRange = 1) => {
+    const neuralNet = newNeuralNet(layerCounts, randomRange);
+    neuralNet.layers = layerCounts.slice(1).map((layerCount, i) => newQuadraticLayer(layerCounts[i], layerCount, randomRange));
+    return neuralNet;
+};
+
 const newLinearLayer = (inputSize, outputSize, randomRange, sigma = x => x, dsigma = y => 1) => ({
     weights: Array(outputSize).fill().map(() => Array(inputSize).fill().map(() => initRand(randomRange))),
     bias: Array(outputSize).fill().map(() => [initRand(randomRange)]),
     sigma,
     dsigma,
-    lastOutput: undefined,
+    // lastOutput: undefined,
     pass(input) {
-        // console.log(input.flat());
         this.lastInput = [...input];
-        // console.log(matadd(matMult(this.weights, input), this.bias).flat());
         this.lastOutput = matadd(matMult(this.weights, input), this.bias).map(row => [this.sigma(row[0])]);
         return this.lastOutput;
     },
@@ -61,10 +65,7 @@ const newLinearLayer = (inputSize, outputSize, randomRange, sigma = x => x, dsig
         if (!this.dbias) this.dbias = A.map(() => [0]);
         if (!this.dweights) this.dweights = A.map(() => this.lastInput.map(() => 0));
 
-        // console.log(this.lastInput);
-
         const adjust = A.map((a, y) => a * this.dsigma(this.lastOutput[y][0]));
-        // console.log(adjust);
 
         this.dbias = adjust.map((a, y) => [this.dbias[y][0] + a]);
         this.dweights = adjust.map((a, y) => this.lastInput.map((i, x) => this.dweights[y][x] + i[0] * a));
@@ -78,11 +79,46 @@ const newLinearLayer = (inputSize, outputSize, randomRange, sigma = x => x, dsig
     }
 });
 
+const newQuadraticLayer = (inputSize, outputSize, randomRange) => ({
+    a: randomTensor([outputSize, inputSize, inputSize], randomRange),
+    b: randomTensor([outputSize, inputSize], randomRange),
+    c: randomTensor([outputSize], randomRange),
+    pass(input) {
+        this.lastInput = [...input];
+        this.lastOutput = elementWise([outputSize], ([i]) =>
+            sumOverIndices([inputSize, inputSize], ([j, k]) => this.a.get([i, j, k]) * input[j] * input[k]) +
+            sumOverIndices([inputSize], ([j]) => this.b.get([i, j]) * input[j]) +
+            this.c.get(i)
+        ).data;
+        return this.lastOutput;
+    },
+    getAdjustments(A) {
+        if (!this.da) this.da = zerosTensor([outputSize, inputSize, inputSize]);
+        if (!this.db) this.db = zerosTensor([outputSize, inputSize]);
+        if (!this.dc) this.dc = zerosTensor([outputSize]);
+
+        this.da = elementWise([outputSize, inputSize, inputSize], ([i, j, k]) => A[i] * this.lastInput[j] * this.lastInput[k] + this.da.get([i, j, k]));
+        this.db = elementWise([outputSize, inputSize], ([i, j]) => A[i] * this.lastInput[j] + this.db.get([i, j]));
+        this.dc = elementWise([outputSize], ([i]) => A[i] + this.dc.get(i));
+        return elementWise([inputSize], ([j]) => sumOverIndices([outputSize], ([i]) => this.b.get([i, j]) + sumOverIndices([inputSize], ([k]) => (this.a.get([i, j, k]) + this.a.get([i, k, j])) * this.lastInput[k]))).data;
+    },
+    adjust(alpha) {
+        this.a = elementWise([outputSize, inputSize, inputSize], ([i, j, k]) => this.a.get([i, j, k]) - alpha * this.da.get([i, j, k]));
+        this.a = elementWise([outputSize, inputSize], ([i, j]) => this.b.get([i, j]) - alpha * this.db.get([i, j]));
+        this.a = elementWise([outputSize], ([i]) => this.c.get([i]) - alpha * this.dc.get([i]));
+        delete this.da;
+        delete this.db;
+        delete this.dc;
+    },
+});
+
+// Incomplete
+
 const newConvolutionalNeuralNet = (kernels, randomRange) => ({
     name: Math.random().toString(36).substring(2),
     kernelLayers: kernels.map(([count, width, height, padding, stride]) => Array(count).fill().map(() => newKernel(width, height, randomRange, padding, stride))),
     kernelCounts: kernels,
-    pass(input) {},
+    pass(input) { },
     error(input, target) {
         return 0.5 * this.pass(input).reduce((p, c, i) => p + (c - target[i]) ** 2, 0);
     },
@@ -124,6 +160,8 @@ const newKernel = (width, height, range, padding = 0, stride = 1) => ({
         return output;
     }
 });
+
+// possible activation functions
 
 const sigmoid = x => 1 / (1 + Math.exp(-x));
 const dsigmoid = y => y * (1 - y);
---
+++ tensors.js
@@ -0,0 +1,98 @@
+const newTensor = (dim, data) => {
+    const size = dimSize(dim);
+    if (size !== data.length) throw `${dim} (size: ${size}) does not match data size: ${data.length}!`;
+    if (data.some(d => !(d instanceof Number))) throw `All members in a tensor must be numbers!`;
+    const tensor = zerosTensor(dim);
+    tensor.data = data;
+    return tensor;
+};
+
+const randomTensor = (dim, range = 1) => {
+    const tensor = zerosTensor(dim);
+    const size = dimSize(dim);
+    tensor.data = Array(size);
+    tensor.isRandom = range;
+    return tensor;
+}
+
+const zerosTensor = (dim) => {
+    return {
+        dim,
+        get(index) {
+            if (!(index instanceof Array)) return this.get([index]);
+            const i = createIdx(index, this.dim);
+            if (!this.data) this.data = Array(dimSize(dim));
+            if (this.data[i] === undefined) {
+                if (this.isRandom) this.data[i] = this.isRandom * (Math.random() * 2 - 1);
+                else this.data[i] = 0;
+            }
+            return this.data[i];
+        },
+        set(index, value) {
+            if (typeof index !== 'object') return this.set([index], value);
+            return this.set_byDataIdx(createIdx(index, this.dim), value);
+        },
+        set_byDataIdx(dataIdx, value) {
+            if (typeof value !== 'number') throw `All members in a tensor must be numbers (received: ${value})!`;
+            if (!this.data) {
+                this.data = Array(dimSize(dim));
+            }
+            return this.data[dataIdx] = value;
+        }
+    };
+}
+
+const createIdx = (index, dim) => {
+    if (index.length !== dim.length) throw `Cannot cast ${index} to dimension ${dim}!`;
+    for (const [i, idx] of index.entries()) {
+        if (idx < 0) throw `${idx} is out of range (min: 0)!`;
+        if (idx >= dim[i]) throw `${idx} is out of range (max: ${dim[i] - 1})!`;
+    }
+    return index.reduce((p, idx, i) => p * dim[i] + idx, 0);
+}
+
+const recreateIdx = (i, dim) => {
+    const newDim = [...dim].reverse();
+    return newDim.reduce((p, d) => [Math.floor(p[0] / d), [p[0] % d, ...p[1]]], [i, []])[1];
+}
+
+const dimSize = (dim) => dim.reduce((p, c) => p * c, 1);
+
+const elementWise = (dim, func) => {
+    const tensor = zerosTensor(dim);
+    const size = dimSize(dim);
+    for (let i = 0; i < size; i++) {
+        const index = recreateIdx(i, dim);
+        tensor.set_byDataIdx(i, func(index));
+    }
+    return tensor;
+}
+
+const sumOverIndices = (dim, func) => {
+    let sum = 0;
+    const size = dimSize(dim);
+    for (let i = 0; i < size; i++) {
+        const index = recreateIdx(i, dim);
+        sum += func(index);
+    }
+    return sum;
+}
+
+// const M = 2;
+// const N = 10;
+
+// const a = randomTensor([M, N, N]);
+// const b = randomTensor([M, N]);
+// const c = randomTensor([M]);
+// const x = randomTensor([N]);
+
+// const output = elementWise([M], ([i]) =>
+//     sumOverIndices([N, N], ([j, k]) => a.get([i, j, k]) * x.get(j) * x.get(k)) +
+//     sumOverIndices([N], ([j]) => b.get([i, j]) * x.get(j)) +
+//     c.get(i)
+// );
+
+// console.log(x);
+// console.log(output);
+// console.log(elementWise([M], ([i]) => 2 * c.get(i)))
+// console.log(sumOverIndices([M], ([i]) => c.get(i)));
