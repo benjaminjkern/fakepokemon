bc57277be87c8e7a3c76ada69054dfd2f56eeaad
Ben Kern <benkern@pixelgate.net>
2021-11-15 17:00:51 -0800

    need to finish doing the batches but this is fine for now

--- neuralnet.js
+++ neuralnet.js
@@ -203,154 +203,158 @@ const newKernelLayer = (inputChannels, outputChannels, kernelSpecs, sigma, rando
             );
             return this.lastOutput;
         },
-        getAdjustments(A) {
-            if (!this.dbias) this.dbias = zerosTensor([outputChannels]);
-            const adjust = elementWise([...processedKernelSpecs.outputSize, outputChannels], ([ox, oy, i]) => A.get([ox, oy, i]) * this.sigma(this.lastOutput.get([ox, oy, i]), true));
-            this.dbias = elementWise([outputChannels], ([i]) => sumOverIndices(processedKernelSpecs.outputSize, ([ox, oy]) => this.dbias.get(i) + adjust.get([ox, oy, i])));
-            for (let n = 0; n < outputChannels; n++) {
-                for (let m = 0; m < inputChannels; m++) {
-                    this.kernels[n][m].getAdjustments(adjust, n);
+        getAdjustments(delta) {
+            this.dbias = zerosTensor([outputChannels]);
+
+            const newDelta = delta.map(() => zerosTensor([...processedKernelSpecs.inputSize, inputChannels]));
+            for (const [a, A] of delta.entries()) {
+                const adjust = elementWise([...processedKernelSpecs.outputSize, outputChannels], ([ox, oy, i]) =>
+                    A.get([ox, oy, i]) *
+                    this.sigma(this.lastOutput[a].get([ox, oy, i]), true));
+                this.dbias = elementWise([outputChannels], ([i]) => sumOverIndices(processedKernelSpecs.outputSize, ([ox, oy]) => this.dbias.get(i) + adjust.get([ox, oy, i])));
+                for (let n = 0; n < outputChannels; n++) {
+                    for (let m = 0; m < inputChannels; m++) {
+                        this.kernels[n][m].getAdjustments(adjust, n);
+                    }
                 }
-            }
 
-            const newA = zerosTensor([...processedKernelSpecs.inputSize, inputChannels]);
-            elementWise([inputChannels], ([j]) => this.kernels[0][0].inputOutputLoop(([ox, oy, r, s], kernelIndex) => {
-                newA.set([r, s, j],
-                    newA.get([r, s, j]) +
-                    sumOverIndices([outputChannels], ([i]) => adjust.get([ox, oy, i]) * this.kernels[i][j].adjustment.get([ox, oy, r, s]))
-                )
-            }));
-            return newA;
-        },
-        adjust(alpha) {
-            for (let n = 0; n < outputChannels; n++) {
-                this.bias[n] -= this.dbias.get(n) * alpha;
-                for (let m = 0; m < inputChannels; m++) {
-                    this.kernels[n][m].adjust(alpha);
+                elementWise([inputChannels], ([j]) => this.kernels[0][0].inputOutputLoop(([ox, oy, r, s], kernelIndex) => {
+                    newA.set([r, s, j],
+                        newA.get([r, s, j]) +
+                        sumOverIndices([outputChannels], ([i]) => adjust.get([ox, oy, i]) * this.kernels[i][j].adjustment.get([ox, oy, r, s]))
+                    )
+                }));
+                return newA;
+            },
+            adjust(alpha) {
+                for (let n = 0; n < outputChannels; n++) {
+                    this.bias[n] -= this.dbias.get(n) * alpha;
+                    for (let m = 0; m < inputChannels; m++) {
+                        this.kernels[n][m].adjust(alpha);
+                    }
                 }
-            }
-            delete this.dbias;
-        },
-        gradientLengthSquared() {
-            return sumOverIndices([outputChannels], ([n]) => this.dbias.get(n) ** 2) + sumOverIndices([outputChannels, inputChannels], ([n, m]) => this.kernels[n][m].gradientLengthSquared());
-        },
-        nudge(mutation) {
-            for (let n = 0; n < outputChannels; n++) {
-                this.bias[n] += mutation * (Math.random() * 2 - 1);
-                for (let m = 0; m < inputChannels; m++) {
-                    this.kernels[n][m].nudge(mutation);
-                }
-            }
-        }
-    }
-};
-
-const default2d = (kernelSpecs, attribute, defaultValue) => {
-    if (kernelSpecs[attribute] === undefined) kernelSpecs[attribute] = [defaultValue, defaultValue];
-    if (typeof kernelSpecs[attribute] === 'number') kernelSpecs[attribute] = [kernelSpecs[attribute], kernelSpecs[attribute]];
-}
-
-const newPoolingLayer = (inputChannels, outputChannels, poolFunc) => { };
-
-const newKernel = (kernelSpecs, randomRange) => {
-    return {
-        ...kernelSpecs,
-
-        kernel: randomTensor(kernelSpecs.kernelSize, randomRange),
-        pass(inputTensor) {
-            this.lastInput = inputTensor;
-            this.lastOutput = zerosTensor(this.outputSize);
-            this.inputOutputLoop(([ox, oy, r, s], kernelIndex) => {
-                this.lastOutput.set([ox, oy], this.lastOutput.get([ox, oy]) +
-                    this.kernel.get(kernelIndex) *
-                    this.lastInput.get([r, s]));
-            });
-            return this.lastOutput;
-        },
-        getAdjustments(A, i) {
-            if (!this.dkernel) {
-                this.dkernel = zerosTensor(this.kernelSize);
-            }
-
-            this.adjustment = zerosTensor([...this.outputSize, ...this.inputSize]);
-            this.inputOutputLoop(([ox, oy, r, s], kernelIndex) => {
-                this.dkernel.set(kernelIndex, this.dkernel.get(kernelIndex) + A.get([ox, oy, i]) * this.lastInput.get([r, s]));
-                this.adjustment.set([ox, oy, r, s], this.kernel.get(kernelIndex));
-            });
-        },
-        inputOutputLoop(func) {
-            for (let ox = 0; ox < this.outputSize[0]; ox++) {
-                for (let oy = 0; oy < this.outputSize[1]; oy++) {
-                    const rMin = Math.max(0, Math.ceil((ox * this.stride[0] - this.padding[0]) / (this.innerPadding[0] + 1)));
-                    const rMax = Math.min(this.inputSize[0], Math.ceil((this.kernelSize[0] + ox * this.stride[0] - this.padding[0]) / (this.innerPadding[0] + 1)));
-                    const sMin = Math.max(0, Math.ceil((oy * this.stride[1] - this.padding[1]) / (this.innerPadding[1] + 1)));
-                    const sMax = Math.min(this.inputSize[1], Math.ceil((this.kernelSize[1] + oy * this.stride[1] - this.padding[1]) / (this.innerPadding[1] + 1)));
-                    for (let r = rMin; r < rMax; r++) {
-                        for (let s = sMin; s < sMax; s++) {
-                            const kernelIndex = [this.padding[0] + (this.innerPadding[0] + 1) * r - ox * this.stride[0], this.padding[1] + (this.innerPadding[1] + 1) * s - oy * this.stride[1]];
-                            func([ox, oy, r, s], kernelIndex);
-                        }
+                delete this.dbias;
+            },
+            gradientLengthSquared() {
+                return sumOverIndices([outputChannels], ([n]) => this.dbias.get(n) ** 2) + sumOverIndices([outputChannels, inputChannels], ([n, m]) => this.kernels[n][m].gradientLengthSquared());
+            },
+            nudge(mutation) {
+                for (let n = 0; n < outputChannels; n++) {
+                    this.bias[n] += mutation * (Math.random() * 2 - 1);
+                    for (let m = 0; m < inputChannels; m++) {
+                        this.kernels[n][m].nudge(mutation);
                     }
                 }
             }
-        },
-        adjust(alpha) {
-            this.kernel = elementWise(this.kernelSize, ([x, y]) => this.kernel.get([x, y]) - alpha * this.dkernel.get([x, y]));
-            delete this.dkernel;
-        },
-        gradientLengthSquared() {
-            return sumOverIndices(this.kernelSize, ([x, y]) => this.dkernel.get([x, y]) ** 2);
-        },
-        nudge(mutation) {
-            elementWise(this.kernelSize, ([x, y]) => {
-                this.kernel.set([x, y], this.kernel.get([x, y]) + mutation * (Math.random() * 2 - 1));
-            });
         }
+    };
+
+    const default2d = (kernelSpecs, attribute, defaultValue) => {
+        if (kernelSpecs[attribute] === undefined) kernelSpecs[attribute] = [defaultValue, defaultValue];
+        if (typeof kernelSpecs[attribute] === 'number') kernelSpecs[attribute] = [kernelSpecs[attribute], kernelSpecs[attribute]];
     }
-};
 
-// possible activation functions, when derivative is set to true, it is expecting the output rather than the input as x.
-// This is just because for many of the functions it is faster to compute the derivative from the output than it is from the input.
+    const newPoolingLayer = (inputChannels, outputChannels, poolFunc) => { };
 
-const sigmoid = (x, derivative) => {
-    if (!derivative) return 1 / (1 + Math.exp(-x));
-    return x * (1 - x);
-}
-const ReLU = (x, derivative) => {
-    if (!derivative) return x > 0 ? x : 0;
-    return x > 0 ? 1 : 0
-}
-const clip = (x, derivative) => {
-    if (!derivative) return Math.max(0, Math.min(1, x));
-    return x === 1 || x === 0 ? 0 : 1;
-}
-const lrelu = (alpha) => (x, derivative) => {
-    if (!derivative) return x > 0 ? x : alpha * x;
-    if (alpha < 0) throw "Leaky Relu cannot determine the derivative from the output when alpha is less than 0!";
-    return x > 0 ? 1 : alpha;
-}
-const tanh = (x, derivative) => {
-    if (!derivative) {
-        if (x >= 19) return 1;
-        if (x <= -19) return -1;
-        const ep = Math.exp(2 * x);
-        return (ep - 1) / (ep + 1);
+    const newKernel = (kernelSpecs, randomRange) => {
+        return {
+            ...kernelSpecs,
+
+            kernel: randomTensor(kernelSpecs.kernelSize, randomRange),
+            pass(inputTensor) {
+                this.lastInput = inputTensor;
+                this.lastOutput = zerosTensor(this.outputSize);
+                this.inputOutputLoop(([ox, oy, r, s], kernelIndex) => {
+                    this.lastOutput.set([ox, oy], this.lastOutput.get([ox, oy]) +
+                        this.kernel.get(kernelIndex) *
+                        this.lastInput.get([r, s]));
+                });
+                return this.lastOutput;
+            },
+            getAdjustments(A, i) {
+                if (!this.dkernel) {
+                    this.dkernel = zerosTensor(this.kernelSize);
+                }
+
+                this.adjustment = zerosTensor([...this.outputSize, ...this.inputSize]);
+                this.inputOutputLoop(([ox, oy, r, s], kernelIndex) => {
+                    this.dkernel.set(kernelIndex, this.dkernel.get(kernelIndex) + A.get([ox, oy, i]) * this.lastInput.get([r, s]));
+                    this.adjustment.set([ox, oy, r, s], this.kernel.get(kernelIndex));
+                });
+            },
+            inputOutputLoop(func) {
+                for (let ox = 0; ox < this.outputSize[0]; ox++) {
+                    for (let oy = 0; oy < this.outputSize[1]; oy++) {
+                        const rMin = Math.max(0, Math.ceil((ox * this.stride[0] - this.padding[0]) / (this.innerPadding[0] + 1)));
+                        const rMax = Math.min(this.inputSize[0], Math.ceil((this.kernelSize[0] + ox * this.stride[0] - this.padding[0]) / (this.innerPadding[0] + 1)));
+                        const sMin = Math.max(0, Math.ceil((oy * this.stride[1] - this.padding[1]) / (this.innerPadding[1] + 1)));
+                        const sMax = Math.min(this.inputSize[1], Math.ceil((this.kernelSize[1] + oy * this.stride[1] - this.padding[1]) / (this.innerPadding[1] + 1)));
+                        for (let r = rMin; r < rMax; r++) {
+                            for (let s = sMin; s < sMax; s++) {
+                                const kernelIndex = [this.padding[0] + (this.innerPadding[0] + 1) * r - ox * this.stride[0], this.padding[1] + (this.innerPadding[1] + 1) * s - oy * this.stride[1]];
+                                func([ox, oy, r, s], kernelIndex);
+                            }
+                        }
+                    }
+                }
+            },
+            adjust(alpha) {
+                this.kernel = elementWise(this.kernelSize, ([x, y]) => this.kernel.get([x, y]) - alpha * this.dkernel.get([x, y]));
+                delete this.dkernel;
+            },
+            gradientLengthSquared() {
+                return sumOverIndices(this.kernelSize, ([x, y]) => this.dkernel.get([x, y]) ** 2);
+            },
+            nudge(mutation) {
+                elementWise(this.kernelSize, ([x, y]) => {
+                    this.kernel.set([x, y], this.kernel.get([x, y]) + mutation * (Math.random() * 2 - 1));
+                });
+            }
+        }
+    };
+
+    // possible activation functions, when derivative is set to true, it is expecting the output rather than the input as x.
+    // This is just because for many of the functions it is faster to compute the derivative from the output than it is from the input.
+
+    const sigmoid = (x, derivative) => {
+        if (!derivative) return 1 / (1 + Math.exp(-x));
+        return x * (1 - x);
     }
-    return 1 - x ** 2;
-}
-const linear = (x, derivative = false) => {
-    if (!derivative) return x;
-    return 1;
-}
+    const ReLU = (x, derivative) => {
+        if (!derivative) return x > 0 ? x : 0;
+        return x > 0 ? 1 : 0
+    }
+    const clip = (x, derivative) => {
+        if (!derivative) return Math.max(0, Math.min(1, x));
+        return x === 1 || x === 0 ? 0 : 1;
+    }
+    const lrelu = (alpha) => (x, derivative) => {
+        if (!derivative) return x > 0 ? x : alpha * x;
+        if (alpha < 0) throw "Leaky Relu cannot determine the derivative from the output when alpha is less than 0!";
+        return x > 0 ? 1 : alpha;
+    }
+    const tanh = (x, derivative) => {
+        if (!derivative) {
+            if (x >= 19) return 1;
+            if (x <= -19) return -1;
+            const ep = Math.exp(2 * x);
+            return (ep - 1) / (ep + 1);
+        }
+        return 1 - x ** 2;
+    }
+    const linear = (x, derivative = false) => {
+        if (!derivative) return x;
+        return 1;
+    }
 
-// Todo: Make work
-// const dropout = (func, rate) =>  (x, derivative) => {
-//     if (Math.random() < rate) return 0;
-//     return func(x, derivative)
-// }
+    // Todo: Make work
+    // const dropout = (func, rate) =>  (x, derivative) => {
+    //     if (Math.random() < rate) return 0;
+    //     return func(x, derivative)
+    // }
 
-// This is just for convenience, if someone wants to create their own activation function all they need to do is provide a forward function and an activation
-const createActivationFunc = (reg, deriv) => (x, derivative = false) => {
-    if (!derivative) return reg(x);
-    return deriv(x);
-}
+    // This is just for convenience, if someone wants to create their own activation function all they need to do is provide a forward function and an activation
+    const createActivationFunc = (reg, deriv) => (x, derivative = false) => {
+        if (!derivative) return reg(x);
+        return deriv(x);
+    }
